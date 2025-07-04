5c62b332212aeba341ee25daf9c6a96763c075af
AbhinavBest <abhinavdeotale1289@gmail.com>
2025-07-04 17:03:26 +0530

    Send Correlation ID#2

--- app/main.py
+++ app/main.py
@@ -10,16 +10,17 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    conn, addr = server.accept() # wait for client
-    print(f"Accepted connection from {addr}")
+    while True:
+        conn, addr = server.accept() # wait for client
+        print(f"Accepted connection from {addr}")
 
-    message_id = struct.pack(">i",0)
-    correlation_id = struct.pack(">i",7)
+        message_id = struct.pack(">i",0)
+        correlation_id = struct.pack(">i",7)
 
-    response = message_id + correlation_id
+        response = message_id + correlation_id
 
-    conn.sendall(response)
-    conn.close()
+        conn.sendall(response)
+        conn.close()
 
 if __name__ == "__main__":
     main()
